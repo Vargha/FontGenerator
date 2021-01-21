ded0d03e7d152b16e711065b7feafa8145c41ff0
Vargha <vhokmran@extensis.com>
2021-01-21 15:41:55 -0800

    Initial commit generate chars jpg.

---
+++ FontToImage.py
@@ -0,0 +1,37 @@
+from PIL import Image, ImageFont, ImageDraw
+import glob
+from fontTools.ttLib import TTFont
+
+
+def has_glyph(font, glyph):
+    for table in font['cmap'].tables:
+        if ord(glyph) in table.cmap.keys():
+            return True
+    return False
+
+
+def main():
+    text = 'A'
+    for font_path in glob.glob('fonts/*.*'):
+        try:
+            background = Image.new('RGB', (36, 36), color='white')
+            if font_path.lower().endswith('ttf'):
+                try_font = TTFont(font_path)
+                if not has_glyph(try_font, text):
+                    continue
+
+            if font_path.lower().endswith('.ttf') or font_path.lower().endswith('.otf'):
+                print(font_path)
+                font = ImageFont.truetype(font_path, 28)
+                image_editable = ImageDraw.Draw(background)
+                image_editable.text((0, 0), text, (0, 0, 0), font=font)
+                result_dir = 'result'
+                background.save(result_dir + '/' + font_path.lower().split('fonts/')[1].split('.ttf')[0].split('.otf')[0] + '.JPG')
+        except:
+            print("That's Bizarre!")
+
+
+if __name__ == "__main__":
+    print('Starting the app...')
+    main()
+    print('Finished successfully!')
